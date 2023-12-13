3f53198de613dc598d71ba25571b5c0c09a3098e
Charles Luguda <charlesluguda@yahoo.com>
2023-12-13 22:35:30 +0300

    Startup Website

---
+++ app.js
@@ -0,0 +1,8 @@
+//Frequent Asked Questions
+
+const accordion = document.getElementsByClassName('faq-container');
+for(i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+}
